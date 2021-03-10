61c0e1ade9aef407bdd043bef37dc665cbd802ad
Santiago Granobles <santiago.granobles@hotmail.com>
2021-03-09 19:32:41 -0500

    avanzando en el main, clases listas(creo)

--- Earth Protectors/src/Main.java
+++ Earth Protectors/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.core.PFont;
 
@@ -24,11 +26,18 @@ public class Main extends PApplet {
 
 	Screen inicio;
 	Player player;
-	Blast baku;
+	Blast blast;
 	PFont miFuente, miFuente2, miFuente3, miFuente4, miFuente5;
+
+	private ArrayList<Enemy> enemyList;
 
 	@Override
 	public void setup() {
+
+		inicio = new Screen(0, 0, this);
+		player = new PlayerShoots((width / 2), 600, this);
+		enemyList = new ArrayList<Enemy>();
+		blast = new Blast(0, 0, this);
 
 		miFuente5 = createFont("Montserrat-Black.ttf", 20);
 		miFuente4 = createFont("Montserrat-BlackItalic.ttf", 20);
@@ -40,9 +49,80 @@ public class Main extends PApplet {
 	public void draw() {
 
 		background(255);
-		
-		
-		
+		paintScreen();
+		drawButtonSelect();
+		drawBacktHome();
+		time();
+		score();
+
+		paintEnemy();
+		initEnemy();
+
+		drawPlayer();
+		removeEnemy();
+
 	}
 
+	private void paintScreen() {
+		switch (screenChange) {
+		case 0:
+			inicio.drawHome(this);
+			break;
+		case 1:
+			inicio.drawGame(this);
+			break;
+		case 2:
+			inicio.drawInstru(this);
+			break;
+
+		case 4:
+			inicio.drawStatus(this);
+			finishGame();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private void drawPlayer() {
+		if (screenChange==1) {
+			player.draw(this);
+		}
+	}
+	
+	private void initEnemy() {
+		if(screenChange==0) {
+			frameCount=0;
+		}
+		if(screenChange==1) {
+			frameRate(60);
+			if (frameCount == 120) {
+				int posX = (int) random(-50, 10);
+				enemyList.add(new Enemy(posX, 150, this));
+				System.out.println(enemyList.size());
+				frameCount = 0;
+			}
+		}
+	}
+	private void paintEnemy() {
+		if(screenChange==1) {
+		for (int i = 0; i < enemyList.size(); i++) {
+			enemyList.get(i).draw(this);
+			enemyList.get(i).mov(this);
+			}
+		}
+	}
+
+	private void finishGame() {
+		textFont(miFuente);
+		text(score, 230, 322);
+		text(m + ":" + s, 230, 367);
+		text(defeat, 370, 408);
+		for (int i = 0; i < enemyList.size(); i++) {
+			if (dist(blast.getPosX(), blast.getPosY(), enemyList.get(i).getPosX(), enemyList.get(i).getPosY()) < 50) {
+				defeat = defeat + 1;
+			}
+		}
+
+	}
 }
--- Earth Protectors/src/PlayerShoots.java
+++ Earth Protectors/src/PlayerShoots.java
@@ -1,4 +1,80 @@
+import java.util.ArrayList;
 
-public class PlayerShoots {
+import processing.core.PApplet;
 
+public class PlayerShoots extends Player{
+
+private ArrayList<Blast> Blast;
+	
+	public PlayerShoots(int posX, int posY, PApplet app) {
+		super(posX, posY, app);
+		Blast = new ArrayList<>();
+	}
+	
+	@Override
+	public void draw(PApplet app) {
+		for (int i = 0; i < Blast.size(); i++) {
+			Blast.get(i).draw(app);
+		}
+		
+		app.imageMode(app.CENTER);
+		app.image(Personaje, posX, posY);
+		app.imageMode(app.CORNER);
+		
+	}
+
+	@Override
+	public void mov(PApplet app) {
+		switch (app.key) {
+		case 'a' :
+			this.posX-= 100;
+			if (this.posX<100) {
+				this.posX=100;
+			}
+			break;
+			
+		case 'd' :
+			this.posX+= 100;
+			if (this.posX>1100) {
+				this.posX=1100;
+			}
+			break;
+		case 'w' :
+			this.posY-= 50;
+			if (this.posY<550) {
+				this.posY=550;
+			}
+			break;
+			
+		case 's' :
+			this.posY+= 50;
+			if (this.posY>600) {
+				this.posY=600;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void mousePressed() {
+		shoot();
+	}
+	
+	private void shoot() {
+		Blast b = new Blast(this.posX, this.posY,app);
+		Blast.add(b);
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public ArrayList<Blast> getBaku() {
+		return Blast;
+	}
 }
